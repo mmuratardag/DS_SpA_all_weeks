d9d1354c3665afd64a34d535f2027c88d23019fd
M. Murat Ardag <mahmut.murat.ardag@uol.de>
2021-02-03 18:25:39 +0100

    committing all files I worked on 3rd Feb

---
+++ 02_03/keep_collecting_lyrics/pe_lyrics/pe_lyrics.py
@@ -0,0 +1,46 @@
+
+"""
+How to install scrapy:
+pip install scrapy OR conda install scrapy
+
+How to run this script:
+>>> scrapy runspider -L WARNING allthelyrics.py
+
+To output to a file:
+>>> scrapy runspider -L WARNING -o rolling_stones.csv allthelyrics.py
+"""
+
+import scrapy
+from scrapy.http import Request
+import re
+
+
+class SongSpider(scrapy.Spider):
+    name = "Pennywise Scraper"
+    allowed_domains = ["www.allthelyrics.com"]
+    start_urls = ["https://www.allthelyrics.com/de/lyrics/pennywise"]
+    custom_settings = {
+        'DEPTH_LIMIT': 10,
+        'AUTOTHROTTLE_ENABLED': False,
+        #see: https://docs.scrapy.org/en/latest/topics/autothrottle.html#autothrottle-extension
+
+    }
+
+    def parse(self, response):
+        songlinks = response.xpath(
+            "//li[contains(@class, 'lyrics-list-item')]/a/@href"
+        ).getall()
+
+        for sl in songlinks:
+            yield Request(response.urljoin(sl))
+
+        song_text = response.xpath(
+            "//div[contains(@class, 'content-text')]//p//text()"
+        ).getall()
+
+        clean_text = "".join(song_text)
+        clean_text = re.sub("\n", " ", clean_text)
+
+        item = {"lyrics": clean_text, "artist": "pennywise"}
+        print(item)
+        yield item

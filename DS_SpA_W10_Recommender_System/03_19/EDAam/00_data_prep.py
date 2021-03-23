2b9209d77dc87ae4cd7373a0e9a9a58276ab4dee
M Murat Ardag <mahmut.murat.ardag@uol.de>
2021-03-23 16:46:05 +0100

    update to the current version

--- DS_SpA_W10_Recommender_System/03_19/EDAam/00_data_prep.py
+++ DS_SpA_W10_Recommender_System/03_19/EDAam/00_data_prep.py
@@ -2,17 +2,11 @@
 import pandas as pd
 
 movies =  pd.read_csv('data/movies.csv')
-# movies.shape # (9742, 3)
 ratings = pd.read_csv('data/ratings.csv')
-# ratings.shape # (100836, 4)
-# movies['movieId'].nunique() # 9742
-# ratings['userId'].nunique() # 610
-
 
 df = pd.merge(ratings, movies, how="left", on=["movieId"])
 genres_expanded = df["genres"].str.split("|", n = 5, expand = True)
 genres_expanded = genres_expanded.rename(columns={0: "Genre0", 1: "Genre1", 2:"Genre2", 3:"Genre3", 4:"Genre4", 5:"Genre5"})
-
 
 df = pd.concat([df, genres_expanded], axis=1)
 df = df.drop('genres', axis=1)
